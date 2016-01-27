563bdbaab8598a3dcdcd63a5c3aaa7f4ca66e066
Kate Hall and Simon Temple <simonctemple@live.com>
2016-01-27 11:07:46 -0800

    create functionality for randomWord and getLength prototypes with passing specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 function HangMan(word) {
-  this.word= word;
+  this.word = word;
 }
+
+// passed test prototype for specific word
+HangMan.prototype.randomWord = function() {
+  return this.word;
+};
+
+HangMan.prototype.getLength = function() {
+  return this.word.length;
+};
+
+// HangMan.prototype.randomWord = function() {
+//   var wordSelector = ["labrador", "poodle", "boston terrier"];
+//     var randomNum = Math.floor((Math.random() * 3));
+//       return wordSelector[randomNum];
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,33 @@
 
 describe("HangMan", function(){
-   it("will randomly select a word from the array", function() {
-   var testHangMan = new HangMan("dog");
-     expect(testHangMan.word).to.equal("dog");
-   });
+  it("will return a word", function() {
+    var testHangMan = new HangMan("dog");
+    expect(testHangMan.word).to.equal("dog");
+  });
+
+  it("will return a word", function() {
+    var testHangMan = new HangMan("dog");
+    expect(testHangMan.randomWord()).to.equal("dog");
+  });
+
+  it("will determine the length of selected word", function() {
+    var testHangMan = new HangMan("dog");
+    expect(testHangMan.getLength()).to.equal(3);
+  });
 });
+
+
+
+
+ // script for random word selector
+  //  it("will randomly a word from array with working prototype", function() {
+  //    var testHangMan = new HangMan(["labrador", "poodle", "boston terrier"]);
+  //    expect(testHangMan.randomWord()).to.equal("labrador" || "poodle" || "boston terrier");
+  //  });
+
+
+
+
 
 
 // describe('vowelReplace', function() {
