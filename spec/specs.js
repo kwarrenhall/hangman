dbb857dca00748ff0e03de6da488e3051419b16f
Kate Hall and Simon Temple <kwarrenhall@gmail.com>
2016-01-27 08:20:39 -0800

    create initial files for pig dice project

---
+++ spec/specs.js
@@ -0,0 +1,11 @@
+
+describe('vowelReplace', function() {
+  it("will replace vowel a with -", function() {
+    expect(vowelReplace("a")).to.equal("-");
+  });
+
+// describe('getRemainder', function(){
+//   it("returns output array of counter added to itself until reaching input1 ", function(){
+//     expect(getRemainder(30, 5)).to.equal(5, 10, 15, 20, 25, 30);
+//   });
+// });
