77250813900ae0647bb57f40a0dd477624b0956f
Kate Hall and Simon Temple <kwarrenhall@gmail.com>
2016-01-27 14:49:30 -0800

    add blanks array length equal to random word length

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,44 @@ function HangMan(word) {
   this.word = word;
 }
 
-// passed test prototype for specific word
-HangMan.prototype.randomWord = function() {
-  return this.word;
-};
-
-HangMan.prototype.getLength = function() {
-  return this.word.length;
-};
-
 // interface
 debugger;
+
+var blanks = [];
+blanks = ourword.length;
 var userInput = "a";
 var ourWord = "labrador";
 userInput = userInput.toLowerCase();
 var outPut;
 for (var i = 0; i < ourWord.length; i++) {
   if (userInput === ourWord.charAt(i)) {
-    // console.log(ourWord.charAt([i]))
-    // outPut = ourWord.replace(/[userInput]/g, ourWord.charAt([i]));
-    console.log("test pass")
+    outPut = letters.push(i)
   }
 }
 
+var userInput;
+var blanks = [];
+var letters = [];
 
-// HangMan.prototype.randomWord = function() {
-//   var wordSelector = ["labrador", "poodle", "boston terrier"];
-//     var randomNum = Math.floor((Math.random() * 3));
-//       return wordSelector[randomNum];
-// }
+var randomWord = function() {
+  var wordSelector = ["labrador", "poodle", "boston terrier"];
+  var randomNum = Math.floor((Math.random() * 3));
+  var randomWord =  wordSelector[randomNum];
+  var letters = []
+    for (var i = 0; i < randomWord.length; i++) {
+      letters = randomWord.split("");
+    }
+  return letters;
+};
+ blanks.length = randomWord().length;
+ for (var i = 0; i < randomWord.length; i++) {
+   if (userInput === randomWord.charAt(i)) {
+     blanks[i] = randomWord.charAt(i);
+   };
+ };
+
+// var randomWord = "cat";
+// var letters = []
+//   for (var i = 0; i < randomWord.length; i++) {
+//     letters = randomWord.split("");
+//   }
